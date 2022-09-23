ed211e04e1fbbf780f7f62c734c896cbf43134dc
AnjasTa <anjas.k@travancoreanalytics.com>
2022-09-23 10:26:30 +0530

    file upload frontend

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,38 @@
 import React, { useEffect, useState } from "react";
 import "../../styles/home.css";
 import Navbar from "../../components/navbar";
+import { Button,message } from "antd";
+import { environment } from "../../environments";
+import axios from "axios";
 
 export default function Home() {
-  const [loginDetails,setloginDetails] = useState(Object)
+  const [loginDetails,setloginDetails] = useState(Object);
+  const [userImage,SetUserImage] = useState<any>();
 
   useEffect(() => {
     let objectData: any = localStorage.getItem("loginDetails");
     setloginDetails(JSON.parse(objectData));
   }, []);
 
-  // const fileUpload = ((event:any)=>{
-  //   const file = event.target.files[0];
-  // })
+  const fileUpload = ((event:any)=>{
+    SetUserImage(event.target.files[0]) 
+  })
+
+  const uploadPhoto = ()=>{
+    const formData = new FormData();
+    formData.append('file', userImage);
+    const headers = {
+      "Content-Type": "multipart/form-data",
+      "Access-Control-Allow-Origin": "*",
+    };
+    axios.post(environment.baseUrl+'singleFile', formData,{headers:headers})
+    .then(res => {
+       message.success("photo upload successfully")
+    })
+    .catch(err => {
+       console.log(err);
+    });
+  }
  
   return (
     <div>
@@ -24,12 +44,15 @@ export default function Home() {
       <div className="userData">
         <h3>Email : {loginDetails.email}</h3>
       </div>
-      {/* <div className="userData" style={{paddingTop:"1rem"}}>
+      <div className="userData" style={{paddingTop:"1rem"}}>
         <h3>Upload your photo</h3>
       </div>
-      <div className="userData">
-        <input style={{paddingLeft:'2rem'}} type="file" onChange={fileUpload} />
-      </div> */}
+      <div className="userData" style={{paddingRight:"1rem"}}>
+        <input style={{paddingLeft:'6rem'}} type="file" onChange={fileUpload} accept="image/*" />
+      </div>
+      <div className="userData" style={{marginTop:"1rem"}}>
+      <Button onClick={uploadPhoto}>Upload Photo</Button>
+      </div>
    
     </div>
   );
